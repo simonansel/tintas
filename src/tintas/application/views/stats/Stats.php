20f61d9593500c71cbb302db156b2f1262a90cb1
simonansel <contact@simon-ansel.fr>
2017-12-15 10:33:37 +0100

    [Update] Modification du style de la page Statistiques

--- src/tintas/application/views/stats/Stats.php
+++ src/tintas/application/views/stats/Stats.php
@@ -3,40 +3,34 @@
         <legend>Mes statistiques</legend>
     </fieldset>
 
-        <table class="table table-striped table-bordered">
-            <thead>
-            <tr>
-                <th>
-                    Parties jouées
-                </th>
-                <th>
-                    Parties gagnées
-                </th>
-                <th>
-                    Egalités
-                </th>
-            </tr>
-            </thead>
-            <tbody>
-                    <tr>
-                        <td>
-                            <div class="input-group">
-                                <input class="form-control" type="text" name="nbPlayed" value="<?php echo $stats->nbPlayed ?>" disabled>
-                            </div>
-                        </td>
-                        <td>
-                            <div class="input-group">
-                                <input class="form-control" type="text" name="nbWin" value="<?php echo $stats->nbWin ?>" disabled>
-                            </div>
-                        </td>
-                        <td>
-                            <div class="input-group">
-                                <input class="form-control" type="text" name="nbDraw" value="<?php echo $stats->nbDraw ?>" disabled>
-                            </div>
-                        </td>
-                    </tr>
-            </tbody>
-        </table>
+    <table class="table table-striped table-bordered">
+        <thead>
+        <tr>
+            <th>
+                Parties jouées
+            </th>
+            <th>
+                Parties gagnées
+            </th>
+            <th>
+                Egalités
+            </th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr>
+            <td class="col-md-4">
+                <div class="alert alert-info text-center" ><?php echo $stats->nbPlayed ?></div>
+            </td>
+            <td class="col-md-4">
+                <div class="alert alert-success text-center" ><?php echo $stats->nbWin?></div>
+            </td>
+            <td class="col-md-4">
+                <div class="alert alert-warning text-center" ><?php echo $stats->nbDraw?></div>
+            </td>
+        </tr>
+        </tbody>
+    </table>
 
 </div>
 
